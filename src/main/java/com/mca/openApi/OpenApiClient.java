b3584a569bf19d6381e3799c18dd6cca5e599893
Ahmed Riahi <contact@ahmedriahi.net>
2022-11-22 22:43:42 +0100

    oauth (#2)

--- src/main/java/com/mca/openApi/OpenApiClient.java
+++ src/main/java/com/mca/openApi/OpenApiClient.java
@@ -3,31 +3,39 @@ package com.mca.openApi;
 import io.swagger.oas.models.OpenAPI;
 import io.swagger.oas.models.PathItem;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
-import java.util.HashMap;
+import java.net.URI;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class OpenApiClient {
 
     private OpenAPI openAPISpec;
 
-    public OpenApiClient(OpenAPI openAPISpec) {
+    private String serviceName;
+
+    private WebClient webClient;
+
+    public OpenApiClient(WebClient webClient,String serviceName, OpenAPI openAPISpec) {
+        this.webClient = webClient;
         this.openAPISpec = openAPISpec;
+        this.serviceName = serviceName;
     }
 
     public Object call(PathItem.HttpMethod httpMethod, String pathName, Map<String, Object> requestParams, Map<String, Object> pathVariables, Object requestBody) {
-        PathItem pathItem = this.openAPISpec.getPaths().get(pathName);
 
-        WebClient webClient = WebClient.create(this.openAPISpec.getServers().get(0).getUrl());
-        WebClient.RequestBodySpec requestBodySpec = webClient
+        WebClient.RequestBodySpec requestBodySpec = this.webClient
                 .method(HttpMethod.resolve(httpMethod.name()))
                 .uri(uriBuilder -> {
-                    uriBuilder = uriBuilder.path(pathName);
+                    URI uri = URI.create(this.openAPISpec.getServers().get(0).getUrl());
+                    uriBuilder = uriBuilder
+                            .scheme(uri.getScheme())
+                            .host(uri.getHost())
+                            .port(uri.getPort())
+                            .path(pathName);
                     if (requestParams != null && !requestParams.isEmpty()) {
                         MultiValueMap<String, String> stringRequestParams = new LinkedMultiValueMap();
                         for (Map.Entry<String, Object> entry : requestParams.entrySet()) {
@@ -38,10 +46,11 @@ public class OpenApiClient {
                     return uriBuilder.build(pathVariables);
                 });
 
-
         if (requestBody != null) {
             requestBodySpec.bodyValue(requestBody);
         }
+
+        requestBodySpec.attributes(ServerOAuth2AuthorizedClientExchangeFilterFunction.clientRegistrationId(this.serviceName));
         WebClient.ResponseSpec responseSpec = requestBodySpec.retrieve();
 
         return responseSpec.bodyToMono(String.class).block();
--- src/main/java/com/mca/openApi/OpenApiLoader.java
+++ src/main/java/com/mca/openApi/OpenApiLoader.java
@@ -2,6 +2,7 @@ package com.mca.openApi;
 
 
 
+import com.mca.openApi.exception.OpenApiLoadException;
 import io.swagger.oas.models.OpenAPI;
 import io.swagger.parser.models.ParseOptions;
 import io.swagger.parser.v3.OpenAPIV3Parser;
@@ -13,6 +14,10 @@ import java.util.ArrayList;
 public class OpenApiLoader {
 
     public OpenAPI loadSpecFile(String url) {
-        return new OpenAPIV3Parser().readLocation(url, new ArrayList<>(),new ParseOptions()).getOpenAPI();
+        OpenAPI openAPI = new OpenAPIV3Parser().readLocation(url, new ArrayList<>(),new ParseOptions()).getOpenAPI();
+        if(openAPI == null) {
+            throw new OpenApiLoadException(url);
+        }
+        return openAPI;
     }
 }
